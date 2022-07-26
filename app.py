57f6ecec8e965b93fa17a55e6e1dac8f1ab89086
Boaz Yoo <blazeryoo1921@gmail.com>
2022-07-26 18:44:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host='0.0.0.0', port =80, debug=True, threaded=True)
