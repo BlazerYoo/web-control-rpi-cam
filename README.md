a5918bc32810acd4425529cc95b0866a1ff2c3d2
Boaz Yoo <blazeryoo1921@gmail.com>
2022-07-26 18:16:47 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # web control rpi cam
  
+## Adapted from
+https://medium.com/@rovai/raspberry-pi-cam-pan-tilt-controlled-over-local-internet-49ecad3a5ee8
+
+https://www.geeksforgeeks.org/flask-form-submission-without-page-reload/
+
+https://stackoverflow.com/questions/21825777/how-to-get-button-value-by-ajax
---
+++ app.py
@@ -0,0 +1,53 @@
+import os
+from tiem import sleep
+from flask import Flask, render_template, request, Response
+from markupsafe import escape
+from camera_pi import Camera
+
+app = Flask(__name__)
+
+# Global variables definition and initialization
+global panServoAngle
+global tiltServoAngle
+panServoAngle = 0
+tiltServoAngle = 0
+
+panPin = 27
+tiltPin = 17
+
+
+@app.route("/",methods=["POST","GET"])
+def home():
+	if request.method == "POST":
+		direction = escape(request.form.get("direction"))
+		print(direction)
+		if 'up' in direction.lower():
+			tiltServoAngle += 10
+		elif 'down' in direction.lower():
+			tiltServoAngle -= 10
+		elif 'left' in direction.lower():
+			panServoAngle -= 10
+		else:
+			panServoAngle += 10
+
+		os.system("python angleServoCtrl.py " + str(panPin) + " " + str(panServoAngle))
+
+		os.system("python angleServoCtrl.py " + str(tiltPin) + " " + str(tiltServoAngle))
+
+	return render_template('home.html')
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+	app.run(debug=True)
