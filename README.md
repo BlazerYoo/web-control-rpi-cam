70ef7dccd04265b067f103e42673506cf4481e36
Boaz Yoo <blazeryoo1921@gmail.com>
2022-07-31 21:28:53 -0400

    smoother servo motion

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ https://medium.com/@rovai/raspberry-pi-cam-pan-tilt-controlled-over-local-intern
 https://www.geeksforgeeks.org/flask-form-submission-without-page-reload/
 
 https://stackoverflow.com/questions/21825777/how-to-get-button-value-by-ajax
+
+https://github.com/garyexplains/examples/tree/master/servo
--- app.py
+++ app.py
@@ -1,53 +1,69 @@
 import os
 from time import sleep
-from flask import Flask, render_template, request, Response
+from flask import Flask, request, render_template
 from markupsafe import escape
-from camera_pi import Camera
+from gpiozero import Servo
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+
+def init():
+    os.system("sudo pigpiod")
+    factory = PiGPIOFactory()
+    global panServo
+    global tiltServo
+    global panServoAngle
+    global tiltServoAngle
+    panServo = Servo(27, min_pulse_width=0.6/1000, max_pulse_width=2.7/1000, pin_factory=factory)
+    tiltServo = Servo(17, min_pulse_width=0.5/1000, max_pulse_width=2.4/1000, pin_factory=factory)
+    panServoAngle = -1
+    tiltServoAngle = -1
+    panServo.value = panServoAngle
+    tiltServo.value = tiltServoAngle
+
 
 app = Flask(__name__)
 
-# Global variables definition and initialization
-global panServoAngle
-global tiltServoAngle
-panServoAngle = 0
-tiltServoAngle = 0
 
-panPin = 27
-tiltPin = 17
-
-
-@app.route("/",methods=["POST","GET"])
+@app.route("/", methods=["POST", "GET"])
 def home():
-	if request.method == "POST":
-		direction = escape(request.form.get("direction"))
-		print(direction)
-		if 'up' in direction.lower():
-			tiltServoAngle += 10
-		elif 'down' in direction.lower():
-			tiltServoAngle -= 10
-		elif 'left' in direction.lower():
-			panServoAngle -= 10
-		else:
-			panServoAngle += 10
+    global panServo
+    global tiltServo
+    global panServoAngle
+    global tiltServoAngle
+    interval = 0.08
+    if request.method == "POST":
+        direction = escape(request.form.get("direction"))
+        print(direction)
+        if 'up' in direction.lower():
+            if (tiltServoAngle + interval) >= 1:
+                tiltServoAngle = 1
+            else:
+                tiltServoAngle += interval
+            tiltServo.value = tiltServoAngle
+        elif 'down' in direction.lower():
+            if (tiltServoAngle - interval) <= -1:
+                tiltServoAngle = -1
+            else:
+                tiltServoAngle -= interval
+            tiltServo.value = tiltServoAngle
+        elif 'left' in direction.lower():
+            if (panServoAngle + interval) >= 1:
+                panServoAngle = 1
+            else:
+                panServoAngle += interval
+            panServo.value = panServoAngle
+        else:
+            if (panServoAngle - interval) <= -1:
+                panServoAngle = -1
+            else:
+                panServoAngle -= interval
+            panServo.value = panServoAngle
+            
+        print("PAN:", panServoAngle, "TILT:", tiltServoAngle)
 
-		os.system("python angleServoCtrl.py " + str(panPin) + " " + str(panServoAngle))
+    return render_template('home.html')
 
-		os.system("python angleServoCtrl.py " + str(tiltPin) + " " + str(tiltServoAngle))
-
-	return render_template('home.html')
-
-def gen(camera):
-    """Video streaming generator function."""
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+    init()
+    app.run(host='0.0.0.0', port =80, debug=True, threaded=True)
